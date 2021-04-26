62a78b51beb824001416e40ad6fa4fe568a5b738
jy1643 <82298763+jy1643@users.noreply.github.com>
2021-04-26 19:06:53 +0900

    Delete main2.py

--- main2.py
+++
@@ -1,9 +0,0 @@
-import genshinstats as gs
-import os
-gs.set_cookie(account_id = int(os.environ.get('ACCOUNT_ID2')), cookie_token = os.environ.get('COOKIE_TOKEN2'))
-
-info = gs.get_daily_reward_info()
-print('total rewards claimed:',info['total_sign_day'])
-print(gs.sign_in()) # signed you in, returns a bool whether it succeeded
-info = gs.get_daily_reward_info()
-print('total rewards claimed:',info['total_sign_day']) # check-in completed if increased
