7e5d8827063ac47856fdcc12b452750e3271d0f2
hammyang <52337069+hammyang@users.noreply.github.com>
2021-06-19 10:17:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ gs.set_cookie(ltuid = int(os.environ.get('LTUID')), ltoken = os.environ.get('LTO
 info = gs.get_daily_reward_info()
 print('total rewards claimed:',info[1])
 print("Get Reward :", gs.sign_in()) # signed you in, returns a bool whether it succeeded
-info = gs.get_daily_reward_info()
-print('total rewards claimed:',info['total_sign_day']) # check-in completed if increased
+print(gs.claim_daily_reward())
+print('total rewards claimed:',info[1]) # check-in completed if increased
