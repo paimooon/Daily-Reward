6bb3177c69655ca3df9b2f42b3db6512d4c8629b
paimooon <81897465+paimooon@users.noreply.github.com>
2021-06-30 01:54:31 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-import genshinstats as gs
+import requests
 import os
-gs.set_cookie(ltuid = int(os.environ.get('LTUID')), ltoken = os.environ.get('LTOKEN'))
 
-info = gs.get_daily_reward_info()
-print('total rewards claimed:',info[1])
-print("Get Reward :", gs.claim_daily_reward()) # signed you in, returns a bool whether it succeeded
-info = gs.get_daily_reward_info()
-print('total rewards claimed:',info[1]) # check-in completed if increased
+ltuid = os.environ.get('LTUID')
+ltoken = os.environ.get('LTOKEN')
+
+url = "https://hk4e-api-os.mihoyo.com/event/sol/sign?act_id=e202102251931481&lang=ko-kr"
+
+cookies = {'ltuid': ltuid, 'ltoken': ltoken}
+
+r = requests.post(url, cookies=cookies)
+print(r.json()['message'])
